45e1457a8f19e9d4c0bec2317f06b41dab6fbbab
foggybrains <dustin.keyes@gmail.com>
2019-05-27 17:41:42 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?  
+    i = 0
+    new_obj = []
+    while i < array.length
+        new_obj << yield(array[i])
+        i = i + 1
+    end
+    new_obj
+  else
+    nil
+  end
+end
